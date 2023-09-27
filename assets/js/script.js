6e964a1a411fe018be6238bbd272d410ae11efd1
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2023-09-28 03:14:51 +0545

    thedailyceller

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/*
+ *   NavBar toggler for mobile
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
